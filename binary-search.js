e7ff249138fc78584b7ca2f70c77f1554d461e56
Walter Chavarria <wchavarria03@gmail.com>
2018-06-05 22:22:17 -0600

    Adding binary search tests

---
+++ binary-search.js
@@ -0,0 +1,27 @@
+function binarySearchLoop(list, ele) {
+  var min = 0;
+  var max = list.length - 1;
+
+  while(min <= max) {
+    var mid = (min + max) / 2;
+    var guess = list[mid];
+
+    if(ele === guess) {
+      return mid;
+    } else if(ele < guess) {
+      max = mid - 1;
+    } else {
+      min = mid + 1;
+    }
+  }
+  return null;
+}
+
+function binarySearchRecursive(array, ele) {
+  return null;
+}
+
+module.exports = {
+  binarySearchLoop,
+  binarySearchRecursive
+};
---
+++ binary-search.test.js
@@ -0,0 +1,33 @@
+const { binarySearchLoop, binarySearchRecursive } = require('./binary-search');
+let list;
+
+beforeEach(() => {
+  list = [1, 2, 3, 5, 6, 7, 9, 11, 20, 25, 31, 46, 59, 75, 83, 89, 90];
+});
+
+describe('Binary Search', () => {
+  describe('Loop', () => {
+    test('Random Case', () => {
+      expect(binarySearchLoop(list, 59)).toBe(12);
+    });
+    test('Worst Case', () => {
+      expect(binarySearchLoop(list, 9)).toBe(6);
+    });
+    test('Not Found Case', () => {
+      expect(binarySearchLoop(list, 9)).toBeNull();
+    });
+  });
+  describe('Recursive', () => {
+    test('Random Case', () => {
+      expect(binarySearchRecursive(list, 59)).toBe(12);
+    });
+    test('Worst Case', () => {
+      expect(binarySearchRecursive(list, 9)).toBe(6);
+    });
+    test('Not Found Case', () => {
+      expect(binarySearchRecursive(list, 9)).toBeNull();
+    });
+  });
+});
+
+
